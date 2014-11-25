bd270a8580618c0fb405e5185fc2d5cca232849b
Jeff Wang <jeffreywang93@gmail.com>
2014-11-25 00:27:42 -0800

    made global functions for cube and quad

--- Code/globals.js
+++ Code/globals.js
@@ -26,3 +26,46 @@ var up = vec3(0, 1, 0);
 // stores objects of all the data
 // object name
 var objectData = []
+
+function Cube(vertices, points, normals, uv, scale){
+    Quad(vertices, points, normals, uv, 0, 1, 2, 3, vec3(0, 0, 1), scale);
+    Quad(vertices, points, normals, uv, 4, 0, 6, 2, vec3(0, 1, 0), scale);
+    Quad(vertices, points, normals, uv, 4, 5, 0, 1, vec3(1, 0, 0), scale);
+    Quad(vertices, points, normals, uv, 2, 3, 6, 7, vec3(1, 0, 1), scale);
+    Quad(vertices, points, normals, uv, 6, 7, 4, 5, vec3(0, 1, 1), scale);
+    Quad(vertices, points, normals, uv, 1, 5, 3, 7, vec3(1, 1, 0 ), scale);
+}
+
+function Quad( vertices, points, normals, uv, v1, v2, v3, v4, normal, scale){
+
+    normals.push(normal);
+    normals.push(normal);
+    normals.push(normal);
+    normals.push(normal);
+    normals.push(normal);
+    normals.push(normal);
+
+    if (!scale) {
+        uv.push(vec2(0,0));
+        uv.push(vec2(1,0));
+        uv.push(vec2(1,1));
+        uv.push(vec2(0,0));
+        uv.push(vec2(1,1));
+        uv.push(vec2(0,1));
+    } else {
+        uv.push(vec2(0,0));
+        uv.push(vec2(2,0));
+        uv.push(vec2(2,2));
+        uv.push(vec2(0,0));
+        uv.push(vec2(2,2));
+        uv.push(vec2(0,2));
+    }
+
+    points.push(vertices[v1]);
+    points.push(vertices[v3]);
+    points.push(vertices[v4]);
+    points.push(vertices[v1]);
+    points.push(vertices[v4]);
+    points.push(vertices[v2]);
+}
+
